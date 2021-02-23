21110e0be5362c7c20e5823428149fd751ed3d95
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-02-23 23:17:10 +0600

    Header done and i use menu for copy and search bar like popup and all complete header

--- js/main.js
+++ js/main.js
@@ -2,4 +2,48 @@ $('documnet').ready(function() {
 
   new WOW().init(); /*wow start*/
   
+  $('.navbar-toggler').sidr({
+        name: 'asidr-main',
+        source: '#asidr',
+        displace: false,
+        renaming: false,
+    });
+
+    $('.sidr-close').on('click', function(){
+        $.sidr('close', 'asidr-main');
+    });
+
+    $('body').swipe( {
+        //Single swipe handler for left swipes
+        swipeLeft: function () {
+            $.sidr('close', 'asidr-main');
+        },
+        swipeRight: function () {
+            $.sidr('open', 'asidr-main');
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold: 45
+    });
+
+    $('.navbar-toggler').on('click',function(){
+        $('.show-overly').addClass('active');
+    })
+
+    $('.sidr-close').on('click',function(){
+        $('.show-overly').removeClass('active');
+    })
+
+    $('.search-triger').on('click',function(){
+        $('.search-bar').addClass('active');
+    })
+
+    $('.search-close').on('click',function(){
+        $('.search-bar, .show-overly').removeClass('active');
+    })
+
+    $('.search-triger').on('click',function(){
+        $('.show-overly').addClass('active');
+    })
+
+
 });
