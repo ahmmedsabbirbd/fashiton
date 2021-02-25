58003ed0953e26d42de6b81f3f14e9e0a6242c02
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-02-25 13:15:53 +0600

    Complete slick-slider coustom design and complete website without responsive

--- js/main.js
+++ js/main.js
@@ -17,33 +17,41 @@ $('documnet').ready(function() {
         //Single swipe handler for left swipes
         swipeLeft: function () {
             $.sidr('close', 'asidr-main');
+            $('.show-overly').removeClass('active');
         },
+        // swipeLeft: function () {
+        //   $('.show-overly').removeClass('active');
+        // },
         swipeRight: function () {
             $.sidr('open', 'asidr-main');
+            $('.show-overly').addClass('active')
         },
+        /* swipeRight: function () {
+            $('.show-overly').addClass('active');
+          },*/
         //Default is 75px, set to 0 for demo so any distance triggers swipe
         threshold: 45
-    });
+    }); 
 
     $('.navbar-toggler').on('click',function(){
         $('.show-overly').addClass('active');
-    })
+    });
 
     $('.sidr-close').on('click',function(){
         $('.show-overly').removeClass('active');
-    })
+    });
 
     $('.search-triger').on('click',function(){
         $('.search-bar').addClass('active');
-    })
+    });
 
     $('.search-close').on('click',function(){
         $('.search-bar, .show-overly').removeClass('active');
-    })
+    });
 
     $('.search-triger').on('click',function(){
         $('.show-overly').addClass('active');
-    }) 
+    });
     
     $('.product-list').isotope({
       // set itemSelector so .grid-sizer is not used in layout
@@ -51,6 +59,14 @@ $('documnet').ready(function() {
       percentPosition: true,
       masonry: { 
       }
-    })
+    });
+
+
+    $('.slick-slider').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
 
 });
